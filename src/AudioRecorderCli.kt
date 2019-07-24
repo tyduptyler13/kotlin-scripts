5f4790d371bee455ef5cc0792735aa40fd2893a9
Tyler Scott <tyduptyler13@gmail.com>
2019-07-24 16:07:34 -0600

    Added AudioRecorder (the first script)

---
+++ src/AudioRecorderCli.kt
@@ -0,0 +1,168 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
+import com.googlecode.lanterna.SGR
+import com.googlecode.lanterna.TerminalPosition
+import com.googlecode.lanterna.TextColor
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.io.File
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+
+/**
+ * @author Tyler Scott
+ */
+class AudioRecorderCli : CliktCommand(
+    printHelpOnEmptyArgs = true,
+    help = """This program is designed to record short pieces of audio very quickly. It will setup your microphone and wait to begin. From there, you press space to start and every time you press space it will save the last segment to disk with a numbered value. 
+
+Keys:
+  <space> - Start/Save
+  q - Quit (don't save)
+  r - Retry (clears buffer and starts this segment over)
+  x - Deletes the last saved recording
+  p - Plays the last saved recording
+  s - Stops recording (without saving)
+  
+Version:
+  1.0.1
+"""
+) {
+    val prompts: File by argument(help = "A csv of prompt text (col 1)").file(
+        exists = true,
+        fileOkay = true,
+        readable = true
+    )
+
+    val skipExisting: Boolean by option(help = "Allows you to continue if you have previously stopped by skipping inputs for which the output already exists").flag()
+
+    var currentMessage = "Press space to start"
+
+    val promptValues: List<String> by lazy {
+        val parser = CSVParser(
+            prompts.bufferedReader(), CSVFormat.DEFAULT.withSkipHeaderRecord(true)
+                .withFirstRecordAsHeader()
+        )
+
+        parser.records.map {
+            it[0]
+        }
+    }
+
+    var index = -1
+
+    override fun run() {
+        AudioRecordingAPI().use { api ->
+            runTerminalLoop(api)
+        }
+
+    }
+
+    private fun runTerminalLoop(api: AudioRecordingAPI) {
+        DefaultTerminalFactory().createTerminal().use { terminal ->
+            TerminalScreen(terminal).use { screen ->
+                screen.startScreen()
+                screen.cursorPosition = null
+
+                var dirty = true
+
+                while (true) {
+                    if (screen.terminalSize != terminal.terminalSize) {
+                        dirty = true
+                    }
+                    screen.doResizeIfNecessary()
+
+                    screen.pollInput()?.let {
+                        dirty = true
+                        screen.clear()
+                        when (it.character) {
+                            ' ' -> {
+                                showNextMessage()
+                                api.startRecording(currentMessage.replace(' ', '_'))
+                            }
+                            'r' -> {
+                                val prefix = currentMessage.replace(' ', '_')
+                                api.stopRecording()
+                                api.deleteLastFile(prefix)
+                                api.startRecording(prefix)
+                            }
+                            'x' -> {
+                                api.stopRecording()
+                                api.deleteLastFile(currentMessage.replace(' ', '_'))
+                                currentMessage = "Deleted last recording"
+                            }
+                            'p' -> {
+                                if (index - 1 < 0) {
+                                    currentMessage = "No previous recordings"
+                                }
+                                api.playLastFile(promptValues[index - 1].replace(' ', '_'))
+                                currentMessage = "Playing..."
+                            }
+                            's' -> {
+                                api.stopRecording()
+                                currentMessage = "Stopped"
+                            }
+                            'q' -> return
+                        }
+                    }
+
+                    if (dirty || api.recording) {
+                        screen.clear()
+                        screen.showKeymap()
+                        screen.showCenteredMessage()
+                        if (api.recording) {
+                            screen.showRecording()
+                            screen.showRecordingTime(api.recordingStartTime!!)
+                        }
+                        screen.refresh()
+                    }
+
+                    Thread.sleep(17) // 60ish fps
+                    dirty = false
+                }
+            }
+        }
+    }
+
+    private fun showNextMessage() {
+        index++
+        currentMessage = promptValues[index]
+    }
+
+
+    private fun TerminalScreen.showCenteredMessage() {
+        newTextGraphics().putString(
+            TerminalPosition(terminalSize.columns / 2 - currentMessage.length / 2, terminalSize.rows / 2),
+            currentMessage
+        )
+    }
+
+    private fun TerminalScreen.showKeymap() {
+        val message =
+            "q - Quit, <space> - Save and continue, r - Retry, x - Delete last recording, p - Play last recording, s - Stop recording"
+        newTextGraphics().putString(TerminalPosition(0, terminalSize.rows - 1), message)
+    }
+
+    private fun TerminalScreen.showRecording() {
+        newTextGraphics()
+            .setBackgroundColor(TextColor.ANSI.RED)
+            .setForegroundColor(TextColor.ANSI.BLACK)
+            .setModifiers(EnumSet.of(SGR.BLINK))
+            .putString(TerminalPosition(0, 0), "Recording")
+    }
+
+    private fun TerminalScreen.showRecordingTime(startTime: Instant) {
+        val duration = Duration.between(startTime, Instant.now())
+        val timeString = "${duration.seconds}:${duration.nano}"
+        newTextGraphics()
+            .putString(TerminalPosition(terminalSize.columns - timeString.length, 0), timeString)
+    }
+}
+
+fun main(args: Array<String>) = AudioRecorderCli().main(args)
---
+++ src/AudioRecordingAPI.kt
@@ -0,0 +1,86 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.Instant
+import javax.sound.sampled.*
+
+class AudioRecordingAPI : AutoCloseable {
+
+    private val line: TargetDataLine
+
+    init {
+        val format = AudioFormat(16000f, 16, 1, true, false)
+        val info = DataLine.Info(TargetDataLine::class.java, format)
+
+        if (!AudioSystem.isLineSupported(info)) {
+            error("No supported audio recording device")
+        }
+
+        line = AudioSystem.getLine(info) as TargetDataLine
+    }
+
+    var recording = false
+
+    var recordingStartTime: Instant? = null
+
+    var currentRecordingThread: Thread? = null
+
+    var clip: Clip? = null
+
+    fun stopRecording() {
+        stopPlaying()
+        line.stop()
+        currentRecordingThread!!.join()
+    }
+
+    fun startRecording(filePrefix: String) {
+        stopPlaying()
+        if (recording) {
+            line.stop() // The thread will continue to exist until it finishes writing to that file
+        }
+
+        recording = true
+        recordingStartTime = Instant.now()
+
+        currentRecordingThread = Thread {
+            line.open()
+            line.start()
+
+            val ais = AudioInputStream(line)
+
+            val path = Paths.get("$filePrefix.wav")
+            Files.deleteIfExists(path)
+
+            AudioSystem.write(ais, AudioFileFormat.Type.WAVE, path.toFile())
+        }
+
+        currentRecordingThread!!.start()
+    }
+
+    override fun close() {
+        line.close()
+        clip?.close()
+    }
+
+    fun deleteLastFile(filePrefix: String) {
+        stopPlaying()
+        Files.delete(Paths.get("$filePrefix.wav"))
+    }
+
+    fun playLastFile(filePrefix: String) {
+        val ais = AudioSystem.getAudioInputStream(Paths.get("$filePrefix.wav").toFile().inputStream())
+
+        clip = AudioSystem.getClip()
+
+        clip!!.open(ais)
+
+        clip!!.start()
+    }
+
+    private fun stopPlaying() {
+        clip?.let {
+            it.stop()
+            it.close()
+        }
+        clip = null
+    }
+}
